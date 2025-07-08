a188224d3a64534fe4c681c41182146689557747
unknown <pablo@climasens.com>
2025-07-08 10:29:25 +0200

    Sharing final data folder through stable drive file in institutional account

--- src/NYCF_data_download.R
+++ src/NYCF_data_download.R
@@ -4,6 +4,6 @@ require("googledrive")
 ## You will be required to sign up with a Google Drive account and grant access to the tidyverse library
 ## You will also be asked for permission to cache your google drive credentials, to which you may answer "No" ("2") in order to prevent them from being saved in your R profile.
 
-drive_download(as_id("1qM3Immes6gOiGxdXguuvS8-m_6s5HQUb"), path = "data.zip")
+drive_download(as_id("1BGL2lXki-ZY6_WyqblaU4Gf0zFUZ3Jhb"), path = "data_2.zip")
 
 unzip("data.zip")
